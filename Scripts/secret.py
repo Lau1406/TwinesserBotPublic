43030bd68d4ae603180215f2683581e320306e7b
lau1406 <lkeijzer@live.nl>
2017-01-16 10:10:49 +0100

    initial commit

---
+++ Scripts/secret.py
@@ -0,0 +1,7 @@
+USERNAME = ''
+PASSWORD = ''
+__consumer_key__ = ''
+__consumer_secret__ = ''
+__access_token_key__ = ''
+__access_token_secret__ = ''
+__userID__ = ''       # user id the bot runs as, saves effort getting it from the server every time, so make sure its correct
